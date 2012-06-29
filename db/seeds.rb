4f376c3c130f12d961b16feaac72a0b4c59646a7
Nurettin Şenyer <seyyah>
2012-06-29 18:55:45 +0300

    db:seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,16 +1,27 @@
-ClientApplication.delete_all
-User.delete_all
+#ClientApplication.delete_all
+#User.delete_all
+#
+#ClientApplication.create(
+#  :name => "secret",
+#  :app_id => "demo_id",
+#  :app_secret => "demo_secret"
+#)
+#
+#User.create(
+#  :username => "demo",
+#  :password_hash => User.hash_password("password", "salt"),
+#  :password_salt => "salt",
+#  :status => "Active",
+#  :expiration_date => "2020-01-01"
+#)
 
-ClientApplication.create(
-  :name => "secret",
-  :app_id => "demo_id",
-  :app_secret => "demo_secret"
-)
+Client.create(name: "client1",
+              app_id: "YE0NYveQGoFsNLX220Dy5g", 
+              app_secret: "aqpGBedDnHFyp5MmgT8KErr9D015ScmaY8r3vHg5C0")
 
-User.create(
-  :username => "demo",
-  :password_hash => User.hash_password("password", "salt"),
-  :password_salt => "salt",
-  :status => "Active",
-  :expiration_date => "2020-01-01"
-)
+User.create(email: "seyyah@bil.omu.edu.tr",
+            password: "secret",
+            password_confirmation: "secret",
+            first_name: "seyyah",
+            last_name: "name",
+            status: "Active")

bd01e039bfb6858ba05cae1ce2d7fe613240d811
seyyah <nurettin.senyer@bil.omu.edu.tr>
2012-07-01 11:08:43 +0300

    reverse engineering

--- app/controllers/authentications_controller.rb
+++ app/controllers/authentications_controller.rb
@@ -16,6 +16,7 @@ class AuthenticationsController < ApplicationController
 
   def add
     user = User.find(params[:user_id])
+
     if user.valid_password?(params[:user][:password])
       omniauth = session[:omniauth]
       user.authentications.create!(:provider => omniauth['provider'], :uid => omniauth['uid'])
@@ -40,6 +41,7 @@ class AuthenticationsController < ApplicationController
   def create
     omniauth = request.env['omniauth.auth']
     authentication = Authentication.find_by_provider_and_uid(omniauth['provider'], omniauth['uid'])
+
     if authentication
       flash[:notice] = "Signed in successfully"
       sign_in_and_redirect(:user, authentication.user)
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,3 +1,8 @@
 class SessionsController < Devise::SessionsController
+  def after_sign_in_path_for(resource)
+    logger.info { "DEBUG: START: #{session.to_yaml} STOP" }
+    #super
+    session[:user_return_to]
+  end
 end
 

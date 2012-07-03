3b4beca8c942180f1ad620da886d6caeb5adda23
Nurettin Şenyer <seyyah>
2012-07-03 12:13:53 +0300

    db:seed: client IP + secret

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,6 @@
 Client.create(name: "client1",
-              app_id: "YE0NYveQGoFsNLX220Dy5g",
-              app_secret: "aqpGBedDnHFyp5MmgT8KErr9D015ScmaY8r3vHg5C0")
+              app_id: "260765187369663",
+              app_secret: "92383b30045773ed83e99e5cdbce1496")
 
 User.create(email: "seyyah@bil.omu.edu.tr",
             password: "secret",

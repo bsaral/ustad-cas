f7bfd1300326dc96ea8fbb9670e2be056451931b
bsaral <betul.saral@bil.omu.edu.tr>
2012-07-09 11:50:13 +0300

    provider kısmı:ok

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,8 +2,8 @@ Client.create(name: "client1",
               app_id: "260765187369663",
               app_secret: "92383b30045773ed83e99e5cdbce1496")
 
-User.create(email: "seyyah@bil.omu.edu.tr",
-            username: "seyyah",
-            password: "secret",
-            password_confirmation: "secret",
+User.create(email: "betul.saral@bil.omu.edu.tr",
+            username: "bsaral",
+            password: "12345",
+            password_confirmation: "12345",
             status: "Active")
